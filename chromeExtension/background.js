d14c23f0a19d5ecd93c5f03e863e7a2265bc0e51
fkeldh2089 <fkeldh2089@gmail.com>
2023-01-20 18:51:40 +0900

    fix: 크롬 확장 프로그램, api 수정 및 UI 수정

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -7,22 +7,26 @@
 //   console.log(chrome.identity);
 // });
 
-let userInfo;
+// let userInfo;
 // 유저 정보 확인
-chrome.identity.getProfileUserInfo(async (accountInfo) => {
-  console.log(accountInfo);
-  userInfo = accountInfo.email;
-});
-
+// chrome.identity.getProfileUserInfo(async (accountInfo) => {
+//   console.log(accountInfo);
+//   userInfo = accountInfo.email;
+// });
+// tab url 확인
+// chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+//   let url = tabs[0].url;
+//   console.log(url);
+// });
 // popup
-chrome.action.setPopup({ popup: "./popup.html" }, () => {
+chrome.action.setPopup({ popup: "./popup/popup.html" }, () => {
   console.log("pop");
 });
 
 // 설치 시 동작 확인 페이지
-chrome.runtime.onInstalled.addListener(async () => {
-  let url = chrome.runtime.getURL("hello.html");
-  await chrome.tabs.create({ url });
+// chrome.runtime.onInstalled.addListener(async () => {
+//   let url = chrome.runtime.getURL("hello.html");
+//   await chrome.tabs.create({ url });
 
-  console.log(`hello`);
-});
+//   console.log(`hello`);
+// });
